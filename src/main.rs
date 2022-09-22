aac36168c829e0b02eface6d51772aaaceee3535
Joao Lambert <joao.lambert@gft.com>
2022-09-22 02:27:39 -0300

    Good guess game

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    println!("Guess the number!");
+
     let secret_number = rand::thread_rng().gen_range(1..=10);
 
     loop {
@@ -13,7 +15,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
